c1c9eef6f17a8a99a52fe8a81f713818298b7395
novasnuha <78951997+novasnuha@users.noreply.github.com>
2023-07-30 05:43:17 +0700

    Update 300x250.js

--- 300x250.js
+++ 300x250.js
@@ -5,4 +5,4 @@ atOptions = {
 		'width' : 300,
 		'params' : {}
 	};
-	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.highperformancedisplayformat.com/20c7890069d8e41380a2be72f70c6f11/invoke.js"></scr' + 'ipt>');
+	document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.profitabledisplaynetwork.com/20c7890069d8e41380a2be72f70c6f11/invoke.js"></scr' + 'ipt>');
